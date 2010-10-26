79e38abe052faee23c321ff591aa74c07d9756f2
Thomas Lockney <thomas@lockney.net>
2010-10-25 23:30:32 -0700

    Added pseudo Clothesline repo support -- will explain in README (to be added next!)

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,7 +1,9 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
+  val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
   val yoink = "com.codahale" %% "yoink" % "1.1.1-SNAPSHOT" withSources()
+  val clothesline = "Clothesline" % "Clothesline" % "1.0.0-SNAPSHOT"
 }
 
