499fe1f98fb5f194be251f7dc0a4d686a95c20d6
Alex Payne <alex@twitter.com>
2010-11-09 17:21:25 -0800

    update sbt project file with path to yoink dependency that works

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,20 +1,18 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
-
+  // repositories
   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
   val clojureRepo = "Clojure Repository" at "http://build.clojure.org/releases"
 
-
-  val yoink = "com.codahale" %% "yoink" % "1.1.1-SNAPSHOT" withSources()
+  // dependencies
+  val yoink = "com.codahale" % "yoink_2.8.0.RC2" % "1.1.1-SNAPSHOT"
   val clothesline = "Clothesline" % "Clothesline" % "1.0.0-SNAPSHOT"
-
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
   val clojure = "org.clojure" % "clojure" % "1.2.0"
   val clojureContrib = "org.clojure" % "clojure-contrib" % "1.2.0"
   val jettyServer = "org.mortbay.jetty" % "jetty" % "6.1.14"
   val commonsCodec = "commons-codec" % "commons-codec" % "1.4"
-
 }
 
