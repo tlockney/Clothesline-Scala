81f10217552bbb32332ffa23ab99463ee0cfca06
Thomas Lockney <thomas@lockney.net>
2010-11-29 12:30:27 -0800

    Changed version of clothesline to 0.1.0-SNAPSHOT

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,17 +1,22 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
+  val bankSimpleRepoPath = System.getenv("BANKSIMPLE_DEV") + "/BankSimple-Maven-Repo"
+  
   // repositories
+  val bankSimpleSnapshotRepo = "BankSimple Snapshot Repository" at "file://" + bankSimpleRepoPath + "/snapshots"
+  val clojureReleaseRepo = "Clojure Release Repository" at "http://build.clojure.org/releases"
+  val clojureSnapshotRepo = "Clojure Snapshot Repository" at "http://build.clojure.org/snapshots"
+  val clojarsRepo = "Clojars Repository" at "http://clojars.org/repo/"
   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
-  val clojureRepo = "Clojure Repository" at "http://build.clojure.org/releases"
+
+  // publishing
+  val publishTo = Resolver.file("BankSimple Snapshot Repository", new java.io.File(bankSimpleRepoPath + "/snapshots"))
 
   // dependencies
-
-  // will eventually want to pull from our local build repo... when that exists
+  val clothesline = "Clothesline" % "Clothesline" % "0.1.0-SNAPSHOT"
   val yoink = "com.codahale" % "yoink_2.8.0.RC2" % "1.1.1-SNAPSHOT"
-
-  val clothesline = "Clothesline" % "Clothesline" % "1.0.0-SNAPSHOT"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
   val liftJson = "net.liftweb" %% "lift-json" % "2.1"
 }
