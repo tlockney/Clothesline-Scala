b08dc4774d4bac65ef65d5d11b6498f32ba69963
Thomas Lockney <thomas@lockney.net>
2011-05-23 13:51:28 -0700

    removed internal dependencies/repositories

--- project/build/project.scala
+++ project/build/project.scala
@@ -2,20 +2,15 @@ import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
 
-  val publishTo = "BankSimple Nexus Snapshots" at "http://nexus.banksimple.com/content/repositories/snapshots"
-
-  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
-
-  override def repositories = Set("BankSimple Nexus Repo" at "http://nexus.banksimple.com/content/groups/public",
-                                  "Local M2 Repo" at "file://" + (Path.userHome/".m2"/"repository").toString)
-
-  override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
+  // repositories
+  val clojars = "clojars.org" at "http://clojars.org/repo"
+  val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
 
   // dependencies
-  val clothesline = "clothesline" % "clothesline" % "0.1.1-SNAPSHOT"
+  val clothesline = "clothesline" % "clothesline" % "0.2.0-SNAPSHOT"
   val yoink = "com.codahale" % "yoink_2.8.0.RC2" % "1.1.1-SNAPSHOT"
+  val logula = "com.codahale" %% "logula" % "2.1.2"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
   val scalaTime = "org.scala-tools.time" % "time_2.8.0" % "0.2"
-  val bsUtil = "com.banksimple" %% "util" % "0.1.0-SNAPSHOT"
 }
 
